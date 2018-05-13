e1580225a36aed28bcbf230a87ac8fa2221e1cba
YukiKuroshima <yukikuro1993@gmail.com>
2018-05-12 17:59:54 -0700

    Done new student/prof/course

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -11,4 +11,60 @@ module.exports.login = function (req, res) {
     }
 }
 
+module.exports.addStudent = function (req, res) {
+  const studentID = req.body.sId;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const name = firstName + " " + lastName;
+  const email  = req.body.email;
+  const major  = req.body.major;
+  const startDate = req.body.startDate;
 
+  const sqlInsert = 'INSERT INTO StudentDimention (StudentKey, StudentID, Name, Major, Email, StartDate) VALUE(?, ?, ?, ?, ?, ?);';
+  const insertValue = [studentID, studentID, name, email, major, startDate];
+
+  conn.query(sqlInsert, insertValue, function (err, calResult) {
+    if (err) {
+      return res.render('error', {message: "Insert error", error: err});
+    }
+    const msg = "Success";
+    return res.render('newStudent', {message: msg});
+  })
+}
+
+module.exports.addProfessor = function (req, res) {
+  const pId = req.body.pId;
+  const name = req.body.name;
+  const email  = req.body.email;
+
+  const sqlInsert = 'INSERT INTO ProfessorDimention (ProfessorKey, ProfID, ProfName, ProfEmail) VALUE(?, ?, ?, ?);';
+  const insertValue = [pId, pId, name, email];
+
+  conn.query(sqlInsert, insertValue, function (err, calResult) {
+    if (err) {
+      return res.render('error', {message: "Insert error", error: err});
+    }
+    const msg = "Success";
+    return res.render('newProfessor', {message: msg});
+  })
+}
+
+module.exports.addCourse = function (req, res) {
+  const cId = req.body.cId;
+  const pId = req.body.pId;
+  const crId = req.body.crId;
+  const cTitle = req.body.courseTitle;
+  const secNum = req.body.sectionNum;
+  const cap  = req.body.cap;
+
+  const sqlInsert = 'INSERT INTO CoursesDimention (CoursesKey, ClassroomID, CourseTitle, `Section#`, ProfID, Capacity) VALUE(?, ?, ?, ?, ?, ?);';
+  const insertValue = [cId, crId, cTitle, secNum, pId, cap];
+
+  conn.query(sqlInsert, insertValue, function (err, calResult) {
+    if (err) {
+      return res.render('error', {message: "Insert error", error: err});
+    }
+    const msg = "Success";
+    return res.render('newCourse', {message: msg});
+  })
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var conn = require("./db");
+const adminCtr = require("../controllers/adminController");
 
 router.get('/addCourse', function (req, res, next) {
     res.render('newCourse');
 })
+router.post('/addCourse', adminCtr.addCourse);
 
 router.get('/addStudent', function (req, res, next) {
     res.render('newStudent');
 })
 
+router.post('/addStudent', adminCtr.addStudent);
+
 router.get('/addProfessor', function (req, res, next) {
     res.render('newProfessor');
 })
+
+router.post('/addProfessor', adminCtr.addProfessor);
 
 module.exports = router;
